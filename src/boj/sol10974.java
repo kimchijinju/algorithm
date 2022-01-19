42c5e622f7524aa8a14045bcf15c215c1eb625a5
kimchijinju <hanbimkim@gmail.com>
2022-01-19 19:54:23 +0900

    22-01-19

--- src/boj/sol10974.java
+++ src/boj/sol10974.java
@@ -6,6 +6,7 @@ public class sol10974 {
     static int N;
     static int[] answer;
     static boolean[] visit;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         N = scanner.nextInt();
@@ -17,14 +18,14 @@ public class sol10974 {
     }
 
     private static void search(int count) {
-        if(count == N) {
-            for(int i : answer)
+        if (count == N) {
+            for (int i : answer)
                 System.out.print(i + " ");
             System.out.println();
         }
 
-        for(int i = 0; i < N; i++) {
-            if(visit[i])
+        for (int i = 0; i < N; i++) {
+            if (visit[i])
                 continue;
             visit[i] = true;
             answer[count] = i + 1;
