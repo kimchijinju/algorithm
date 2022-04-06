10cf19b4502ec51480318ae2c5a0f995548fc96f
kimchijinju <hanbimkim@gmail.com>
2022-04-06 23:16:26 +0900

    22-04-06 행렬 곱셈

---
+++ src/boj/sol2740.java
@@ -0,0 +1,40 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sol2740 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] A = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++)
+                A[i][j] = sc.nextInt();
+        }
+        M = sc.nextInt();
+        int K = sc.nextInt();
+        int[][] B = new int[M][K];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < K; j++)
+                B[i][j] = sc.nextInt();
+        }
+        int[][] result = new int[N][K];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                for (int k = 0; k < M; k++) {
+                    result[i][j] += (A[i][k] * B[k][j]);
+                }
+            }
+        }
+        for (int[] i : result) {
+            for (int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
