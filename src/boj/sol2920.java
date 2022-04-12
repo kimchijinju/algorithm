26d6df74a091d90c1901555945008a4d662162e7
kimchijinju <hanbimkim@gmail.com>
2022-04-12 23:01:08 +0900

    22-04-12

---
+++ src/boj/sol2920.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.util.Scanner;
+
+public class sol2920 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        if(str.equals("1 2 3 4 5 6 7 8"))
+            System.out.println("ascending");
+        else if(str.equals("8 7 6 5 4 3 2 1"))
+            System.out.println("descending");
+        else
+            System.out.println("mixed");
+    }
+}
